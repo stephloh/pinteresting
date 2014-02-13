33ab2ebfbd90002e3e6a327d3786234c695d4863
stephloh <sloh@booster.com>
2014-02-13 21:31:53 +0800

    Cleaned up pins controller file

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,59 @@
+class PinsController < ApplicationController
+  before_action :set_pin, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @pins = Pin.all
+  end
+
+# same like read
+  def show
+  end
+
+#same like create
+  def new
+    @pin = Pin.new
+  end
+
+  def edit
+  end
+
+  def create
+    @pin = Pin.new(pin_params)
+
+      if @pin.save
+        redirect_to @pin, notice: 'Pin was successfully created.' 
+      else
+        render action: 'new' 
+      end
+    end
+  end
+
+
+  def update
+    respond_to do |format|
+      if @pin.update(pin_params)
+       redirect_to @pin, notice: 'Pin was successfully updated.' 
+      else
+       render action: 'edit' 
+      end
+    end
+  end
+
+  def destroy
+    @pin.destroy
+    respond_to do |format|
+      redirect_to pins_url 
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_pin
+      @pin = Pin.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def pin_params
+      params.require(:pin).permit(:description)
+    end
+end
